357d1ece6b8b581c6f669cde83427f3fe5153308
Burak Can <bc913dev@outlook.com>
2021-12-25 01:00:34 -0500

    use mediatr

--- Application/Features/Users/Queries/GetUsers/GetUsersQueryHandler.cs
+++ Application/Features/Users/Queries/GetUsers/GetUsersQueryHandler.cs
@@ -24,7 +24,14 @@ namespace Bcan.Backend.Application.Features.Users.Queries.GetUsers
 
         public async Task<List<UserLiteDto>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
         {
-            var allUsers = await _userRepository.ListAsync(cancellationToken);            
+            //var allUsers = await _userRepository.ListAsync(cancellationToken);
+            var allUsers = await Task.FromResult(new List<User>()
+            {
+                new User(Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}"), new FullName("Burak", " Can"),   "bc913"),
+                new User(Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}"), new FullName("Eren ", "Bekar"),  "eb926"),
+                new User(Guid.Parse("{BF3F3002-7E53-441E-8B76-F6280BE284AA}"), new FullName("Basar", " Ozkurt"), "bo916")
+            });
+
             return _mapper.Map<List<UserLiteDto>>(allUsers);
         }
     }
--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -21,14 +21,8 @@ namespace Bcan.Backend.WebApi.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            //var result = await _mediator.Send(new GetUsersQuery());
-            var allUsers = await Task.FromResult(new List<UserLiteDto>()
-            {
-                new UserLiteDto{ Id = Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}"), FullName = "Burak Can",   NickName = "bc913"},
-                new UserLiteDto{ Id = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}"), FullName = "Eren Bekar",   NickName = "eb926"},
-                new UserLiteDto{ Id = Guid.Parse("{BF3F3002-7E53-441E-8B76-F6280BE284AA}"), FullName = "Basar Ozkurt",  NickName = "bo916"}
-            });
-            return Ok(allUsers);
+            var result = await _mediator.Send(new GetUsersQuery());
+            return Ok(result);
         }
     }
 }
