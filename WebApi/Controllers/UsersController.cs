cbadf1475978b5dee2802e98ab94f2a9e36adfea
Burak Can <bc913dev@outlook.com>
2021-12-30 15:24:54 -0500

    provide authorization for the controller

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,17 +1,23 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using Bcan.Backend.Application.Features.Users.Queries.GetUsers;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Bcan.Backend.WebApi.Controllers
 {
+
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
+    [RequiredScope(scopeRequiredByApi)]
     public class UsersController : ControllerBase
     {
+        internal const string scopeRequiredByApi = "access_as_user";
         private readonly IMediator _mediator;
         public UsersController(IMediator mediator)
         {
