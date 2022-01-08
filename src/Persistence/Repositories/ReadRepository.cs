6f5a4188c439664bdbf78e80a6951ec8d13b2be1
Burak Can <bc913dev@outlook.com>
2022-01-08 00:32:42 -0500

    Implementation for IReadRepo

--- src/Persistence/Repositories/ReadRepository.cs
+++ src/Persistence/Repositories/ReadRepository.cs
@@ -1,17 +1,29 @@
+using Ardalis.GuardClauses;
+using Bcan.Backend.SharedKernel.Contracts;
+using Bcan.Backend.Application.Contracts.Repositories;
+using Bcan.Backend.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Bcan.Backend.SharedKernel.Contracts;
-using Bcan.Backend.Application.Contracts.Repositories;
 
 namespace Bcan.Backend.Persistence.Repositories
 {
     public class ReadRepository<T> : IReadRepository<T> where T : class, IAggregateRootWithId<Guid>
     {
+
+        protected readonly ApplicationDbContext _context;
+
+        public ReadRepository(ApplicationDbContext context)
+        {
+            _context = Guard.Against.Null<ApplicationDbContext>(context, nameof(context));
+        }
+
         public async Task<IReadOnlyCollection<T>> ListAsync(CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return await _context.Set<T>().ToListAsync(cancellationToken);
         }
     }
 }
