48f54c99070bfd91922b1c79363e9417e47751e6
Burak Can <bc913dev@outlook.com>
2022-01-08 00:32:59 -0500

    Implementation for IRepository

---
+++ src/Persistence/Repositories/Repository.cs
@@ -0,0 +1,26 @@
+using Bcan.Backend.SharedKernel.Contracts;
+using Bcan.Backend.Application.Contracts.Repositories;
+using Bcan.Backend.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Bcan.Backend.Persistence.Repositories
+{
+    public class Repository<T> : ReadRepository<T>, IRepository<T> where T : class, IAggregateRootWithId<Guid>
+    {
+
+        public Repository(ApplicationDbContext context) : base(context)
+        {
+            
+        }
+        public async Task<Guid> AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            await _context.Set<T>().AddAsync(entity, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return entity.Id;
+        }
+    }
+}
