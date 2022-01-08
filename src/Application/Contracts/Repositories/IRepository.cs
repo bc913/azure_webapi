110c9665a4947c3d924bf70bfc4d40a1fe9edfca
Burak Can <bc913dev@outlook.com>
2022-01-07 23:06:40 -0500

    Add repo interface for write operations

---
+++ src/Application/Contracts/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bcan.Backend.SharedKernel.Contracts;
+
+namespace Bcan.Backend.Application.Contracts.Repositories
+{
+    public interface IRepository<T> : IReadRepository<T> where T : class, IAggregateRoot
+    {
+        // return id or newly created entity
+        Task<Guid> AddAsync(T entity, CancellationToken cancellationToken = default);
+    }
+}
