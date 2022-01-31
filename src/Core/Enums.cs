f126ab42e874e899aaba5815e885536e3177c0f1
Burak Can <bc913dev@outlook.com>
2022-01-30 21:23:14 -0500

    No need to use Ardalis Enumeration since System.Enum is providing required functionality

--- src/Core/Enums.cs
+++ src/Core/Enums.cs
@@ -1,22 +1,18 @@
 namespace Bcan.Backend.Core
 {
-    public class DanceType : Enumeration
+    public enum DanceType
     {
-        public static DanceType Salsa = new(1, nameof(Salsa));
-        public static DanceType Bachata = new(2, nameof(Bachata));
-        public static DanceType ChaCha = new(3, nameof(ChaCha));
-
-        private DanceType(int id, string name) : base(id, name) { }
+        Salsa,
+        Bachata,
+        ChaCha
     }
 
-    public class DanceLevel : Enumeration
+    public enum DanceLevel
     {
-        public static DanceLevel Beginner = new(1, nameof(Beginner));
-        public static DanceLevel Intermediate = new(2, nameof(Intermediate));
-        public static DanceLevel Advanced = new(3, nameof(Advanced));
-        public static DanceLevel All = new(4, nameof(All));
-
-        private DanceLevel(int id, string name) : base(id, name){}
+        Beginner,
+        Intermediate,
+        Advanced,
+        All
     }
 
     public enum IndividualType
