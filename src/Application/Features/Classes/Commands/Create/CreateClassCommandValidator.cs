86af11d3b20e85f3f43090f74987093a2fb582a9
Burak Can <bc913dev@outlook.com>
2022-02-04 09:42:40 -0500

    Simplify create class command validator

--- src/Application/Features/Classes/Commands/Create/CreateClassCommandValidator.cs
+++ src/Application/Features/Classes/Commands/Create/CreateClassCommandValidator.cs
@@ -1,28 +1,20 @@
 using FluentValidation;
-using Bcan.Backend.Application.Dtos;
-
+using System;
 namespace Bcan.Backend.Application.Features.Classes.Commands.Create
 {
     public class CreateClassCommandValidator : AbstractValidator<CreateClassCommand>
     {
         public CreateClassCommandValidator()
         {
+            // NotEmpty() is handled here bec AutoMapper ignores
+            // the non-default ctor so null check is missing there.
             RuleFor(command => command.Title)
-                .NotEmpty().NotNull().MaximumLength(50);
+                .NotEmpty().MaximumLength(50);
 
-            RuleFor(command => command.Info).DanceInfoMustBeValid();
-
-        }
-    }
-
-    public static class ApplicationValidators
-    {
-        public static IRuleBuilderOptions<T, DanceInfoDto> DanceInfoMustBeValid<T>(this IRuleBuilder<T, DanceInfoDto> ruleBuilder) 
-        {
-
-            return ruleBuilder
-                .Must(info => info.Types is not null).WithMessage("Dance info types should not be null")
-                .Must(info => info.Types.Count > 0).WithMessage("Dance info types should be defined.");
+            RuleFor(command => command.Start)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThan(DateTimeOffset.UtcNow);
         }
     }
 }
