1a08508fde81c35919f32f5a90f984051af3ab44
Burak Can <bc913dev@outlook.com>
2022-02-04 09:43:01 -0500

    Add validation to create class command handler

--- src/Application/Features/Classes/Commands/Create/CreateClassCommandHandler.cs
+++ src/Application/Features/Classes/Commands/Create/CreateClassCommandHandler.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Bcan.Backend.Application.Contracts.Repositories;
+using Bcan.Backend.Application.Exceptions;
 using Bcan.Backend.Core.Entities;
 using MediatR;
 using System;
@@ -20,6 +21,11 @@ namespace Bcan.Backend.Application.Features.Classes.Commands.Create
 
         public async Task<Guid> Handle(CreateClassCommand request, CancellationToken cancellationToken)
         {
+            var validator = new CreateClassCommandValidator();
+            var validationResult = await validator.ValidateAsync(request);
+            if (validationResult.Errors.Count > 0)
+                throw new ValidationException(validationResult);
+
             var classEvent = _mapper.Map<ShineClass>(request);
             return await _repository.AddAsync(classEvent, cancellationToken);
         }
