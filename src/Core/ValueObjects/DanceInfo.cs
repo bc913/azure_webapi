26560b8bf670a505390523596161240da7076c62
Burak Can <bc913dev@outlook.com>
2022-02-05 15:51:37 -0500

    Make use of backing fields for DanceInfo

--- src/Core/ValueObjects/DanceInfo.cs
+++ src/Core/ValueObjects/DanceInfo.cs
@@ -11,14 +11,24 @@ namespace Bcan.Backend.Core.ValueObjects
         public DanceInfo(DanceLevel level, IReadOnlyCollection<DanceType> types)
         {
             Level = level;
-            Types = (IReadOnlyCollection<DanceType>)Guard.Against.NullOrEmpty<DanceType>(
-                types, 
-                nameof(types), 
-                "There should be at least one dance type for a dance info instance.");
+            Types = types;
         }
 
-        public DanceLevel Level { get; private set; }
-        public IReadOnlyCollection<DanceType> Types { get; private set; }
+        private DanceLevel _level;
+        public DanceLevel Level 
+        { 
+            get => _level;
+            private set => _level = value;
+        }
+        private IReadOnlyCollection<DanceType> _types;
+        public IReadOnlyCollection<DanceType> Types 
+        {
+            get => _types;
+            private set => _types = (IReadOnlyCollection<DanceType>)Guard.Against.NullOrEmpty<DanceType>(
+                value, 
+                nameof(Types), 
+                "There should be at least one dance type for a dance info instance.");
+        }
 
         protected override IEnumerable<object> GetEqualityComponents()
         {
