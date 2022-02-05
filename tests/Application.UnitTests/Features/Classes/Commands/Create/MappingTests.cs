728fe61da69d7fd7cc3a7d883b9a71545f47ae38
Burak Can <bc913dev@outlook.com>
2022-02-05 16:55:35 -0500

    Add mapping tests for CreateClassCommand

--- tests/Application.UnitTests/Features/Classes/Commands/Create/MappingTests.cs
+++ tests/Application.UnitTests/Features/Classes/Commands/Create/MappingTests.cs
@@ -75,5 +75,194 @@ namespace Bcan.Backend.Application.UnitTests.Features.Classes.Commands.Create
                 options => options.ExcludingMissingMembers() // Expectation (command) instance has some properties that subject instance does not have so ignore
             );
         }
+
+        [Fact]
+        public void MappingToEntityShouldFailForNullTitle()
+        {
+            // Given
+            var command = new CreateClassCommand
+            {
+                Title = null,
+                Info = _info,
+                Location = FakeLocationDto.Instance,
+                Start = new DateTimeOffset(DateTime.UtcNow).AddDays(12),
+                End = new DateTimeOffset(DateTime.UtcNow).AddDays(13),
+                Fee = _fee,
+                Policy = _policy,
+                Description = "Some class description here"
+            };
+            // When
+            Action act = () => _mapper.Map<ShineClass>(command);
+            // Then (Type of exception is not important since we only care about the failure)
+            act.Should().Throw<AutoMapperMappingException>();
+        }
+
+        [Fact]
+        public void MappingToEntityShouldFailForEmptyTitle()
+        {
+            // Given
+            var command = new CreateClassCommand
+            {
+                Title = string.Empty,
+                Info = _info,
+                Location = FakeLocationDto.Instance,
+                Start = new DateTimeOffset(DateTime.UtcNow).AddDays(12),
+                End = new DateTimeOffset(DateTime.UtcNow).AddDays(13),
+                Fee = _fee,
+                Policy = _policy,
+                Description = "Some class description here"
+            };
+            // When
+            Action act = () => _mapper.Map<ShineClass>(command);
+            // Then (Type of exception is not important since we only care about the failure)
+            act.Should().Throw<AutoMapperMappingException>();
+        }
+
+        [Fact]
+        public void MappingToEntityShouldFailForNullDanceInfo()
+        {
+            // Given
+            var command = new CreateClassCommand
+            {
+                Title = "Some title",
+                Info = null,
+                Location = FakeLocationDto.Instance,
+                Start = new DateTimeOffset(DateTime.UtcNow).AddDays(12),
+                End = new DateTimeOffset(DateTime.UtcNow).AddDays(13),
+                Fee = _fee,
+                Policy = _policy,
+                Description = "Some class description here"
+            };
+            // When
+            Action act = () => _mapper.Map<ShineClass>(command);
+            // Then (Type of exception is not important since we only care about the failure)
+            act.Should().Throw<AutoMapperMappingException>();
+        }
+
+        [Fact]
+        public void MappingToEntityShouldFailForNullLocation()
+        {
+            // Given
+            var command = new CreateClassCommand
+            {
+                Title = "Some title",
+                Info = _info,
+                Location = null,
+                Start = new DateTimeOffset(DateTime.UtcNow).AddDays(12),
+                End = new DateTimeOffset(DateTime.UtcNow).AddDays(13),
+                Fee = _fee,
+                Policy = _policy,
+                Description = "Some class description here"
+            };
+            // When
+            Action act = () => _mapper.Map<ShineClass>(command);
+            // Then (Type of exception is not important since we only care about the failure)
+            act.Should().Throw<AutoMapperMappingException>();
+        }
+
+        [Fact]
+        public void MappingToEntityShouldFailForInvalidDate()
+        {
+            // Given
+            var command = new CreateClassCommand
+            {
+                Title = "Some title",
+                Info = _info,
+                Location = FakeLocationDto.Instance,
+                Start = new DateTimeOffset(DateTime.UtcNow).AddDays(13), //Start date is later than end date
+                End = new DateTimeOffset(DateTime.UtcNow).AddDays(12),
+                Fee = _fee,
+                Policy = _policy,
+                Description = "Some class description here"
+            };
+            // When
+            Action act = () => _mapper.Map<ShineClass>(command);
+            // Then (Type of exception is not important since we only care about the failure)
+            act.Should().Throw<AutoMapperMappingException>();
+        }
+
+        [Fact]
+        public void MappingToEntityShouldFailForNullFee()
+        {
+            // Given
+            var command = new CreateClassCommand
+            {
+                Title = "Some title",
+                Info = _info,
+                Location = FakeLocationDto.Instance,
+                Start = new DateTimeOffset(DateTime.UtcNow).AddDays(12),
+                End = new DateTimeOffset(DateTime.UtcNow).AddDays(13),
+                Fee = null,
+                Policy = _policy,
+                Description = "Some class description here"
+            };
+            // When
+            Action act = () => _mapper.Map<ShineClass>(command);
+            // Then (Type of exception is not important since we only care about the failure)
+            act.Should().Throw<AutoMapperMappingException>();
+        }
+
+        [Fact]
+        public void MappingToEntityShouldFailForNullFeeOptions()
+        {
+            // Given
+            var command = new CreateClassCommand
+            {
+                Title = "Some title",
+                Info = _info,
+                Location = FakeLocationDto.Instance,
+                Start = new DateTimeOffset(DateTime.UtcNow).AddDays(12),
+                End = new DateTimeOffset(DateTime.UtcNow).AddDays(13),
+                Fee = new FeeDto {Options = null},
+                Policy = _policy,
+                Description = "Some class description here"
+            };
+            // When
+            Action act = () => _mapper.Map<ShineClass>(command);
+            // Then (Type of exception is not important since we only care about the failure)
+            act.Should().Throw<AutoMapperMappingException>();
+        }
+
+        [Fact]
+        public void MappingToEntityShouldFailForEmptyFeeOptions()
+        {
+            // Given
+            var command = new CreateClassCommand
+            {
+                Title = "Some title",
+                Info = _info,
+                Location = FakeLocationDto.Instance,
+                Start = new DateTimeOffset(DateTime.UtcNow).AddDays(12),
+                End = new DateTimeOffset(DateTime.UtcNow).AddDays(13),
+                Fee = new FeeDto {Options = null},
+                Policy = _policy,
+                Description = "Some class description here"
+            };
+            // When
+            Action act = () => _mapper.Map<ShineClass>(command);
+            // Then (Type of exception is not important since we only care about the failure)
+            act.Should().Throw<AutoMapperMappingException>();
+        }
+
+        [Fact]
+        public void MappingToEntityShouldFailForNullPolicy()
+        {
+            // Given
+            var command = new CreateClassCommand
+            {
+                Title = "Some title",
+                Info = _info,
+                Location = FakeLocationDto.Instance,
+                Start = new DateTimeOffset(DateTime.UtcNow).AddDays(12),
+                End = new DateTimeOffset(DateTime.UtcNow).AddDays(13),
+                Fee = _fee,
+                Policy = null,
+                Description = "Some class description here"
+            };
+            // When
+            Action act = () => _mapper.Map<ShineClass>(command);
+            // Then (Type of exception is not important since we only care about the failure)
+            act.Should().Throw<AutoMapperMappingException>();
+        }
     }
 }
