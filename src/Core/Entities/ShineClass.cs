358b0171e498bf5d50a282df4fc892bb1ce4fe13
Burak Can <bc913dev@outlook.com>
2022-02-05 15:30:22 -0500

    Make use of backing fields for ShineClass

--- src/Core/Entities/ShineClass.cs
+++ src/Core/Entities/ShineClass.cs
@@ -8,34 +8,83 @@ namespace Bcan.Backend.Core.Entities
 {
     public class ShineClass : BaseEntity<Guid>, IAggregateRootWithId<Guid>
     {
+        /*
+        I use backing fields to maintain invariants during mapping process using AutoMapper.
+        AutoMapper uses reflection and skips the parameter ctor so invariants are broken.
+        Therefore, I'm using the Guards at property-field level to maintain the invariants
+        */
         private ShineClass() : base(Guid.NewGuid()) { }
 
         public ShineClass(Guid id, string title, DanceInfo info,
             Location location, DateTimeOffsetRange time, EventPolicy policy,
             Fee fee, string description = null, Media media = null) : base(id)
         {
-            Title = Guard.Against.NullOrWhiteSpace(title, nameof(title), "Dance class should have a title.");
-            Info = Guard.Against.Null<DanceInfo>(info, nameof(info), "DanceInfo instance can not be null for a dance class.");
-            Location = Guard.Against.Null<Location>(location, nameof(location), "Location should be defined for a dance class.");
-            Time = Guard.Against.Null<DateTimeOffsetRange>(time, nameof(time), "Class should have start and end time defined.");
-            Policy = Guard.Against.Null<EventPolicy>(policy, nameof(policy), "Class should have an event policy defined.");
-            Fee = Guard.Against.Null<Fee>(fee, nameof(fee), "Class definition requires a fee policy to be defined.");
+            Title = title;
+            Info = info;
+            Location = location;
+            Time = time;
+            Policy = policy;
+            Fee = fee;
             Description = description;
             Media = media;
         }
 
-        public string Title { get; private set; }
         public ShineEventType Type { get; private set; } = ShineEventType.Class;
-        public DanceInfo Info { get; private set; }
 
-        public Location Location { get; private set; }
-        public DateTimeOffsetRange Time { get; private set; }
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            private set => _title = Guard.Against.NullOrWhiteSpace(value, nameof(Title), "Dance class should have a title.");
+        }
 
-        public EventPolicy Policy { get; private set; }
-        public Fee Fee { get; private set; }
+        private DanceInfo _info;
+        public DanceInfo Info 
+        {
+            get => _info;
+            private set => _info = Guard.Against.Null<DanceInfo>(value, nameof(Info), "DanceInfo instance can not be null for a dance class.");
+        }
 
-        public string Description { get; private set; }
-        public Media Media { get; private set; }
+        private Location _location;
+        public Location Location 
+        {
+            get => _location;
+            private set => _location = Guard.Against.Null<Location>(value, nameof(Location), "Location should be defined for a dance class.");
+        }
 
+        private DateTimeOffsetRange _time;
+        public DateTimeOffsetRange Time 
+        {
+            get => _time;
+            private set => _time = Guard.Against.Null<DateTimeOffsetRange>(value, nameof(Time), "Class should have start and end time defined.");
+        }
+
+        private EventPolicy _policy;
+        public EventPolicy Policy 
+        {
+            get => _policy;
+            private set => _policy = Guard.Against.Null<EventPolicy>(value, nameof(Policy), "Class should have an event policy defined.");
+        }
+
+        private Fee _fee;
+        public Fee Fee 
+        {
+            get => _fee;
+            private set => _fee = Guard.Against.Null<Fee>(value, nameof(Fee), "Class definition requires a fee policy to be defined.");
+        }
+
+        private string _description;
+        public string Description 
+        {
+            get => _description;
+            private set => _description = value;
+        }
+
+        private Media _media;
+        public Media Media 
+        {
+            get => _media;
+            private set => _media = value;
+        }
     }
 }
