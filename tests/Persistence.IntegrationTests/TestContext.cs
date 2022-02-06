a6f9a07729a65ef093da2ac5a18192d0e9a6838c
Burak Can <bc913dev@outlook.com>
2022-02-06 13:11:50 -0500

    ADd comments for testcontext

--- tests/Persistence.IntegrationTests/TestContext.cs
+++ tests/Persistence.IntegrationTests/TestContext.cs
@@ -5,6 +5,9 @@ namespace Bcan.Backend.Persistence.IntegrationTests
 {
     internal sealed class TestContext : ApplicationDbContext
     {
+        /// <summary>
+        /// See: https://stackoverflow.com/a/60497822
+        /// </summary>
         public TestContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             Database.OpenConnection();
